38ac2671327851dc811d795843fec275f5b60bda
Adithya M Suresh <123856088+AdithyaSuresh01@users.noreply.github.com>
2025-12-11 17:03:56 +0530

    Init __init__.py

---
+++ src/utils/__init__.py
@@ -0,0 +1,7 @@
+"""Utility subpackage for ApiPlaygroundPy.
+
+Currently contains logging helpers, but can be extended later with
+configuration or other cross-cutting utilities.
+"""
+
+from .logger import get_logger  # noqa: F401
